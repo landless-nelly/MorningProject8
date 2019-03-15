98b2fb1f3df4445c857030d0151e76eca7b7dae9
landless-nelly <nellyndunge34@gmail.com>
2019-03-15 12:10:27 +0300

    first commit

---
+++ app/src/main/java/com/nelly/morningproject8/MainActivity.java
@@ -0,0 +1,24 @@
+package com.nelly.morningproject8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+ListView list;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list =findViewById(R.id.tvWatu);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Item "+position+" clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
